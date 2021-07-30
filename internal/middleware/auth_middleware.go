f73fd7459588cc4a6ea5ca9b3b1848885f8f7591
Gleb Mojeico <g.mojeico@gmail.com>
2021-07-30 14:28:00 +0300

    add YOuTruck)

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -59,6 +59,7 @@ func AuthMiddleware(userRepo repository.UserRepo) func(handler http.Handler) htt
 				return
 			}
 
+			// ad user in context
 			ctx := context.WithValue(r.Context(), CurrentUserKey, user)
 
 			next.ServeHTTP(w, r.WithContext(ctx))
@@ -108,6 +109,7 @@ func GetCurrentUserFromContext(ctx context.Context) (*model.User, error) {
 		return nil, errors.New(errNoUserInContext)
 	}
 
+	// get user from context
 	user, ok := ctx.Value(CurrentUserKey).(*model.User)
 	if !ok || user.ID == "" {
 		return nil, errors.New(errNoUserInContext)
